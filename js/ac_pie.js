99bff0ec7b41a2432e7d55a46fc454a675942a6d
younjisu <niganiga@naver.com>
2015-03-10 07:46:24 +0900

    mod. arrange code

--- js/ac_pie.js
+++ js/ac_pie.js
@@ -85,30 +85,6 @@ var ANICHART_PIE = (function() {
           var nDistance = Math.sqrt(Math.pow(htCore.centerX - _x, 2) + Math.pow(htCore.centerY - _y, 2));
           return nDistance;
       },
-      setCompatiblility : function() {
-          //Polyfill reference : https://gist.github.com/paulirish/1579671
-          // var lastTime = 0;
-          // var vendors = ['ms', 'moz', 'webkit', 'o'];
-          // for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-          //     window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-          //     window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
-          //     || window[vendors[x]+'CancelRequestAnimationFrame'];
-          // }
-
-          // if (!window.requestAnimationFrame)
-          //     window.requestAnimationFrame = function(callback, element) {
-          //         var currTime = new Date().getTime();
-          //         var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-          //         var id = window.setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
-          //         lastTime = currTime + timeToCall;
-          //         return id;
-          // };
-
-          // if (!window.cancelAnimationFrame)
-          //     window.cancelAnimationFrame = function(id) {
-          //       clearTimeout(id);
-          //     };
-      },
       getRandomIndex : function(nRandomRange, nNeedCount) {
         var _arr = [];
         while(_arr.length < nNeedCount) {
@@ -153,8 +129,6 @@ var ANICHART_PIE = (function() {
 
   function PIE(elTarget, htOption) {
 
-      //_u.setCompatiblility();
-      //.setCompatiblility();
       _polyfill.exec();
 
       if(!(htOption && typeof htOption === "object")) {
