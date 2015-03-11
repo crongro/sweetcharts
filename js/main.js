6879ba9e458f399051b0e0de86f8c6a538be7899
younjisu <niganiga@naver.com>
2015-03-11 14:48:54 +0900

    mod. rename Directory

--- js/main.js
+++ js/main.js
@@ -24,6 +24,27 @@ require([
                  }
     }).runAnimation();
 
+    var oPy2 = new ANICHART_PIE( document.querySelector(".pieWrap:nth-child(2) svg"), {
+        core : {
+          // centerX:50, //default: center
+          // centerY:200, //default: center 
+          radius:250,
+          nMilliSecondCycle:1000,
+          sRandomColorType : "c" //default: user custom value
+        }, 
+        htPiece : {
+                   "WEBUI"          : {"data" : 23, "color" : "red"},
+                   "WEBServer"      : {"data" : 13, "color" : "blue"},
+                   "DBMaster"       : {"data" : 3,  "color" : "grey"},
+                   "ProjectManager" : {"data" : 43.3, "color" : "green"},
+                   "ProjectManager2" : {"data" : 63.3, "color" : "green"},
+                   "ProjectManager3" : {"data" : 23.3, "color" : "green"},
+                   "Designer"       : {"data" : 13, "color" : "magenta"},
+                   "Designer2"       : {"data" : 23, "color" : "magenta"},
+                   "Designer3"       : {"data" : 33, "color" : "magenta"},
+                 }
+    }).runAnimation();
+
     // var oPy2 = new ANICHART_PIE( document.querySelector(".pieWrap:nth-child(2) svg"), {
     //     core : {
     //       centerX:200,
