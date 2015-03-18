993110ac38e0c4f7c73eba204d50ea9909eda2c7
younjisu <niganiga@naver.com>
2015-03-18 18:38:54 +0900

    mod. arrange for Example page

--- example/main.js
+++ example/main.js
@@ -5,34 +5,34 @@ require.config({
 });
 
 require([
-    "ac_pie"
-], function (ANICHART_PIE) {
+    "ac_pie","ac_donut"
+], function (ANICHART_PIE, Donut) {
 
     if(!isMobile()) {
-      var oPy = new ANICHART_PIE( document.querySelector(".row-first > .chart-wrap:first-child"), {
-        core : {
-          // centerX:50, //default: center
-          // centerY:200, //default: center 
-          radius:150,
-          nMilliSecondCycle:500,
-          //sRandomColorType : "c" //default: user custom value
-        }, 
-        htPiece : {
-         "Chrome(all)"  : {"data" : 46.08, "color" : "green"},
-         "Firefox 5+"   : {"data" : 17.67, "color" : "orange"},
-         "IE(all)"      : {"data" : 20.78, "color" : "dodgerblue"},
-         "safari 7.0"   : {"data" : 10.36, "color" : "skyblue"},
-         "opera"        : {"data" : 2, "color" : "red"},
-       }
-     }).runAnimation();
+     //  var oPy = new ANICHART_PIE( document.querySelector(".row-first > .chart-wrap:first-child"), {
+     //    core : {
+     //      // centerX:50, //default: center
+     //      // centerY:200, //default: center 
+     //      radius:150,
+     //      nMilliSecondCycle:500,
+     //      //sRandomColorType : "c" //default: user custom value
+     //    }, 
+     //    htPiece : {
+     //     "Chrome(all)"  : {"data" : 46.08, "color" : "green"},
+     //     "Firefox 5+"   : {"data" : 17.67, "color" : "orange"},
+     //     "IE(all)"      : {"data" : 20.78, "color" : "dodgerblue"},
+     //     "safari 7.0"   : {"data" : 10.36, "color" : "skyblue"},
+     //     "opera"        : {"data" : 2, "color" : "red"},
+     //   }
+     // }).runAnimation();
 
-      var oPy2 = new ANICHART_PIE( document.querySelector(".row-first > .chart-wrap:last-child "), {
+      var oPy2 = new ANICHART_PIE( document.querySelector(".row-first > .chart-wrap:first-child "), {
         core : {
           // centerX:50, //default: center
           // centerY:200, //default: center 
           radius:150,
           nMilliSecondCycle:500,
-          sRandomColorType : "a" //default: user custom value
+          sRandomColorType : "b" //default: user custom value
         }, 
         htPiece : {
          "WEBUI"          : {"data" : 23, "color" : "red"},
@@ -47,13 +47,13 @@ require([
        }
      }).runAnimation();
 
-      var oPy3 = new ANICHART_PIE( document.querySelector(".row-second > .chart-wrap:first-child "), {
+      var oPy3 = new ANICHART_PIE( document.querySelector(".row-first> .chart-wrap:last-child "), {
         core : {
           // centerX:50, //default: center
           // centerY:200, //default: center 
           radius:150,
-          nMilliSecondCycle:300,
-          sRandomColorType : "b" //default: user custom value
+          nMilliSecondCycle:1000,
+          sRandomColorType : "c" //default: user custom value
         }, 
         htPiece : {
          "WEBUI"          : {"data" : 33, "color" : "red"},
@@ -69,15 +69,12 @@ require([
          "Designer3"       : {"data" : 63, "color" : "magenta"},
        }
      }).runAnimation();
-    }
 
-    var oPy4 = new ANICHART_PIE( document.querySelector(".row-second > .chart-wrap:last-child "), {
+      var oPy_donut = new Donut( document.querySelector(".row-second> .chart-wrap:first-child "), {
         core : {
-          // centerX:50, //default: center
-          // centerY:200, //default: center 
           radius:150,
           nMilliSecondCycle:500,
-          sRandomColorType : "c" //default: user custom value
+          sRandomColorType : "b" //default: user custom value
         }, 
         htPiece : {
                    "WEBUI"          : {"data" : 33, "color" : "red"},
@@ -92,14 +89,37 @@ require([
                    "Designer2"       : {"data" : 43, "color" : "magenta"},
                    "Designer3"       : {"data" : 63, "color" : "magenta"},
                  }
-    }).runAnimation();
+      }).runAnimation();
+
+      var oPy_donut2 = new Donut( document.querySelector(".row-second> .chart-wrap:last-child "), {
+        core : {
+          radius:150,
+          nMilliSecondCycle:1000,
+          sRandomColorType : "d" //default: user custom value
+        }, 
+        htPiece : {
+                   "WEBUI"          : {"data" : 43, "color" : "red"},
+                   "WEBServer"      : {"data" : 23, "color" : "blue"},
+                   "DBMaster"       : {"data" : 12,  "color" : "grey"},
+                   "DBMaster2"       : {"data" : 43,  "color" : "grey"},
+                   "DBMaster3"       : {"data" : 13,  "color" : "grey"},
+                   "ProjectManager" : {"data" : 93.3, "color" : "green"},
+                   "ProjectManager2" : {"data" : 33.3, "color" : "green"},
+                   "ProjectManager3" : {"data" : 23.3, "color" : "green"},
+                   "Designer"       : {"data" : 73, "color" : "magenta"},
+                   "Designer2"       : {"data" : 43, "color" : "magenta"},
+                   "Designer3"       : {"data" : 63, "color" : "magenta"},
+                 }
+      }).runAnimation();
+    }
+
 
     function isMobile() {
       function isMobileAgent() {
-        return !! (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
+        return !!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
       }
       function isMobileWidth() {
-        return !! (window.innerWidth < 401);
+        return !!(window.innerWidth < 401);
       }
       return !! (isMobileAgent() && isMobileWidth());
     }
